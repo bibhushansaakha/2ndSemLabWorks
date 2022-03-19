741584321674342e51cc89fa6781b330501cd0fe
BibhushanSaakha <bibhushansaakha@gmail.com>
2022-03-19 19:45:17 +0545

    Lab 6 Final

--- lab6.cpp
+++ lab6.cpp
@@ -32,10 +32,17 @@ class Triangle : public Shape
 public:
     double side_three;
     void display_area()
-    {
+    {       
         double s=(side_one+side_two+side_three)/2;
-        double area=pow((s*(s-side_one)*(s-side_two)*(s-side_three)),1/2);
-        cout<<"\nArea of the Triangle is:\t"<<area;
+        double area=pow((s*(s-side_one)*(s-side_two)*(s-side_three)),0.5);
+        if (area==0)
+        {
+            cout<<"Invalid Input. The lengths of any two sides of a triangle must add up to more than the length of the third side.";
+        }
+        else
+        {
+            cout<<"\nArea of the Triangle is:\t"<<area;
+        }
     }
 };
 
@@ -51,12 +58,42 @@ public:
 
 int main()
 {
+    int user;
+    Rectangle rect;
     Triangle tip;
-    cout<<"\nEnter side one of the Triangle:\t";
-    cin>>tip.side_one;
-    cout<<"\nEnter side two of the Triangle:\t";
-    cin>>tip.side_two;
-    cout<<"\nEnter side three of the Triangle:\t";
-    cin>>tip.side_three;
-    tip.display_area();
+    while (user!=0)
+    {
+        cout<<"\n\nDo you want to find the area of Triangle(1) or a Rectangle(2) or Quit(0)?\n";
+        cin>>user;
+        if (user==1)
+        {
+            cout<<"\nEnter side one of the Triangle:\t";
+            cin>>tip.side_one;
+            cout<<"\nEnter side two of the Triangle:\t";
+            cin>>tip.side_two;
+            cout<<"\nEnter side three of the Triangle:\t";
+            cin>>tip.side_three;
+            tip.display_area();
+        }
+
+        else if (user==2)
+        {
+            cout<<"\nEnter side one of the Rectangle:\t";
+            cin>>rect.side_one;
+            cout<<"\nEnter side two of the Rectangle:\t";
+            cin>>rect.side_two;
+            rect.display_area();
+        }
+
+        else if (user==0)
+        {
+            cout<<"\nThankyou for using this program!!!\n\n";
+        }
+        
+        else
+        {
+            cout<<"\nInvalid input\n";
+        }
+    }
+          
 }
